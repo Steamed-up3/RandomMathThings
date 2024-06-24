98fff1beea6c7e973ebc46b06f129beceba8ddb5
Steamed-up3 <140709791+Steamed-up3@users.noreply.github.com>
2024-06-24 20:45:15 +0100

    Add files via upload

---
+++ for i in ans i^i.py
@@ -0,0 +1,14 @@
+def math():
+    while True:
+        try:
+            ans = int(input('Enter a number: '))
+            break
+        except ValueError:
+            print('Invalid.\n')
+
+    for i in range(ans):
+        print(i**i)
+
+    print('Done.')
+
+math()
